2ee619b5fdea76d120c66c452898a534a68a6b33
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-06-15 14:16:24 -1000

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -1,29 +1,76 @@
 // create your App component here
 
-// create your App component here
-import React, { Component } from 'react'
 
-class App extends Component {
+import React from "react"
 
-  state = {
-      peopleInSpace: []
-  }
+export default class App extends React.Component{
 
-  render() {
-    return (
-      <div>
-        {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
-      </div>
-    )
-  }
+    state = {
+        peopleInSpace: []
+    }
 
-  componentDidMount() {
-    fetch('http://api.open-notify.org/astros.json')
-      .then(response => response.json())
-      .then(({people}) => this.setState({ peopleInSpace: people }))
+    // componentDidMount() {
+    //     fetch("http://api.open-notify.org/astros.json")
+    //       .then((response) => response.json())
+    //       .then(({data}) => {
+    //         this.setState({
+    //             peopleInSpace: {data}.name,
+    //         });
+    //       });
+    //   }
 
-  }
 
+      componentDidMount() {
+            fetch('http://api.open-notify.org/astros.json')
+              .then(response => response.json())
+              .then(({people}) => this.setState({ peopleInSpace: people }))
+        
+          }
+      
+
+    render(){
+        return (
+                  <div>
+                     {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+                  </div>
+                )
+    }
 }
+    
 
-export default App
+
+
+
+
+
+
+
+
+
+// create your App component here
+// import React, { Component } from 'react'
+
+// class App extends Component {
+
+//   state = {
+//       peopleInSpace: []
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+//       </div>
+//     )
+//   }
+
+//   componentDidMount() {
+//     fetch('http://api.open-notify.org/astros.json')
+//       .then(response => response.json())
+//       .then(({people}) => this.setState({ peopleInSpace: people }))
+
+//   }
+
+// }
+
+// export default App
